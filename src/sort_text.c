daa0c49870089b8d3db0125a243a34709bcdcd7e
Calvin (Kang Min) Seo <kseo6751@uni.sydney.edu.au>
2020-09-11 16:48:07 +1000

    First Commit

---
+++ src/sort_text.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    //Error Handling. Correct number of argument
+    if(argc < 3 || argc > 4) {
+        perror("You have a wrong number of argument\n");
+    }
+
+    
+
+    return 0;
+}
+
